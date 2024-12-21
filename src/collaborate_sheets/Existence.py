dd2676c187ddabe0e7081204b4dc530f7a4e9d1a
IornBird <93601866+IornBird@users.noreply.github.com>
2024-12-21 15:15:55 +0800

    solve space-tab issue in Existence.py
    Signed-off-by: IornBird <93601866+IornBird@users.noreply.github.com>

--- src/collaborate_sheets/Existence.py
+++ src/collaborate_sheets/Existence.py
@@ -1,41 +1,43 @@
+from enum import Enum
+
 class Existance(Enum):
- 	"""
+    """
     formate:
-	    Each type occupies 2 bits
-	        signinific bit makes function cares about it
-		    another bit is this type should exist
-	usage (in function):
-	    Always use & (AND bit operator) to do mask
-	    if result after masked is not 0, do corresponding operation
-	    e.g.
-    	    if option & CARE != 0:
-    	        if option & SHOULD != 0:
-    		        # detect [option exist]
-    	        else:
-    		        # detect [option don't exist]
+        Each type occupies 2 bits
+            signinific bit makes function cares about it
+            another bit is this type should exist
+        usage (in function):
+            Always use & (AND bit operator) to do mask
+            if result after masked is not 0, do corresponding operation
+            e.g.
+                if option & CARE != 0:
+                    if option & SHOULD != 0:
+                        # detect [option exist]
+                    else:
+                        # detect [option don't exist]
     usage (calling function):
-        Use TRUE or FALSE as args
-        e.g. checkExist(TRUE) if you wish this option exists
-	"""
-	CARE   = 0
-	TRUE   = 0
-	FALSE  = 0
-	SHOULD = 0
+    Use TRUE or FALSE as args
+    e.g. checkExist(TRUE) if you wish this option exists
+    """
+    CARE   = 0
+    TRUE   = 0
+    FALSE  = 0
+    SHOULD = 0
 
 class USER(Existance):
-	CARE   = 0b10
-	TRUE   = 0b11
-	FALSE  = 0b10
-	SHOULD = 0b01
+    CARE   = 0b10
+    TRUE   = 0b11
+    FALSE  = 0b10
+    SHOULD = 0b01
 
 class SHEET(Existance):
-	CARE   = 0b10 * 4
-	TRUE   = 0b11 * 4
-	FALSE  = 0b10 * 4
-	SHOULD = 0b01 * 4
+    CARE   = 0b10 * 4
+    TRUE   = 0b11 * 4
+    FALSE  = 0b10 * 4
+    SHOULD = 0b01 * 4
 	
 class ACCESS(Existance):
-	CARE   = 0b10 * 16
-	TRUE   = 0b11 * 16
-	FALSE  = 0b10 * 16
-	SHOULD = 0b01 * 16
+    CARE   = 0b10 * 16
+    TRUE   = 0b11 * 16
+    FALSE  = 0b10 * 16
+    SHOULD = 0b01 * 16
