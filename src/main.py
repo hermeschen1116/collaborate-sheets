e20490eaad5f4ef4717218146231c4a6658cda09
IornBird <93601866+IornBird@users.noreply.github.com>
2024-12-21 11:58:55 +0800

    Become able to check name & sheet input is valid or not
    Signed-off-by: IornBird <93601866+IornBird@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
+from typing import Optional
 from collaborate_sheets.Database import Database
 
-database: Database = Database()
+def checkValid(info: str, tokenNum: int=2) -> tuple[Optional[str]]:
+	"""
+	Check if input, splited by space, is same of tokenNum.
+	:param info: string printed before waiting for input
+	:returns: list of given token, contains None if have error.
+	"""
+	inp = input(info).strip().split()
+	if tokenNum == 2 and len(inp) != 2:
+		print("Invalid input, make sure both user name and sheet name don't contain space.")
+		return None, None
+	elif tokenNum == 1 and len(inp) != 1:
+		print("name should not contain space.")
+		return None,
+	return inp
 
-while True:
-	print("""
+
+
+OPTIONS = """
 ---------------Menu---------------
 1. Create a user
 2. Create a sheet
@@ -13,34 +28,49 @@ while True:
 6. Collaborate with an other user
 7. Quit
 ----------------------------------
-	""")
+"""
 
-	menu_key: str = input("> ")
+if __name__ == '__main__':
+	database: Database = Database()
+	
+	info_sheet = "Input user name and sheet name, splited by space (e.g. \"name title\")\n"
+	
+	while True:
+		print(OPTIONS)
+	
+		menu_key: str = input("> ")
 
-	match menu_key:
-		case "1":
-			user_name: str = input("> ").strip()
-			database.create_user(user_name)
-		case "2":
-			user_name, sheet_title = input("> ").strip().split(" ")
-			database.create_sheet(user_name, sheet_title)
-		case "3":
-			user_name, sheet_title = input("> ").strip().split(" ")
-			database.print_sheet(user_name, sheet_title)
-		case "4":
-			user_name, sheet_title = input("> ").strip().split(" ")
-			database.edit_sheet(user_name, sheet_title)
-		case "5":
-			user_name, sheet_title = input("> ").strip().split(" ")
-			database.change_sheet_accessibility(user_name, sheet_title)
-		case "6":
-			user_name, sheet_title = input("> ").strip().split(" ")
-			database.add_sheet_collaborator(user_name, sheet_title)
-		case "7":
-			print("Goodbye!")
-			break
-		case _:
-			print("Invalid menu key.\n")
+		# check valid input
+		buf: list[str] = []
+		user_name: str = ""
+		if menu_key == "1":
+			buf = checkValid("Input user name without space\n> ", tokenNum=1)
+			user_name = buf[0]
+		elif menu_key in "23456":
+			buf = checkValid(info_sheet + "> ")
+			user_name, sheet_title = buf
+		if user_name is None:
 			continue
+
+		# act based on [menu_key]
+		match menu_key:
+			case "1":
+				database.create_user(user_name)
+			case "2":
+				database.create_sheet(user_name, sheet_title)
+			case "3":
+				database.print_sheet(user_name, sheet_title)
+			case "4":
+				database.edit_sheet(user_name, sheet_title)
+			case "5":
+				database.change_sheet_accessibility(user_name, sheet_title)
+			case "6":
+				database.add_sheet_collaborator(user_name, sheet_title)
+			case "7":
+				print("Goodbye!")
+				break
+			case _:
+				print("Invalid menu key.\n")
+				continue
 
 	print()
